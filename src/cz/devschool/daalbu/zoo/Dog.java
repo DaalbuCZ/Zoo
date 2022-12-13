8a6f263031cce66a13aeedcc583316d4f00c6eeb
Daalbu <daalbu@post.cz>
2022-12-13 20:54:29 +0100

    Add class Dog

---
+++ src/cz/devschool/daalbu/zoo/Dog.java
@@ -0,0 +1,20 @@
+package cz.devschool.daalbu.zoo;
+
+public class Dog extends Animal{
+    private boolean isHuntingMood;
+    public Dog(String name, int age, double weight, boolean isHuntingMood) {
+        super(name, age, weight);
+        this.isHuntingMood = isHuntingMood;
+    }
+
+    @Override
+    public void makeSound() {
+        if(isHuntingMood == true){
+            System.out.println("Vrrrrrr Haf Haf");
+        }else {
+            System.out.println("Haf!");
+        }
+    }
+
+
+}
