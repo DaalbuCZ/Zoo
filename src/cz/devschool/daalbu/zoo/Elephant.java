8c371bbe381b9868c73715d0cfcc752c132225ec
Daalbu <daalbu@post.cz>
2022-12-13 20:57:47 +0100

    Add class Elephant

---
+++ src/cz/devschool/daalbu/zoo/Elephant.java
@@ -0,0 +1,14 @@
+package cz.devschool.daalbu.zoo;
+
+public class Elephant extends Animal{
+
+
+    public Elephant(String name, int age, double weight) {
+        super(name, age, weight);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Frrrrrrrrrr");
+    }
+}
